d969cb097d4f17377d32a116257a052cccd96dea
mpilepic <mpilepic@student.uniri.hr>
2021-12-05 20:05:34 +0100

    Correction

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -19,13 +19,13 @@ function Product({ product }) {
 
                 <Card.Text as="div">
                     <div className="my-3">
-                        <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
+                        <Rating value={product.rating} text={`${product.numReviews} recenzija`} color={'#f8e825'} />
                     </div>
                 </Card.Text>
 
 
                 <Card.Text as="h3">
-                    ${product.price}
+                    {product.price} kn
                 </Card.Text>
             </Card.Body>
         </Card>
